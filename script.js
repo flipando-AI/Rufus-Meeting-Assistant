7902435d23707551e767404c660c8db89ba0dd52
yvesfogel <yvesfogel@gmail.com>
2023-05-23 23:34:05 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,89 @@
+const form = document.getElementById('inputForm');
+const outputText = document.getElementById('promptText');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  updateOutput();
+});
+
+const inputFields = document.querySelectorAll('input[data-hint], textarea[data-hint]');
+
+// Add event listeners for input fields
+inputFields.forEach(function(input) {
+  input.addEventListener('input', function() {
+    updateOutput();
+  });
+  input.addEventListener('focus', function() {
+    showHint(input);
+  });
+  input.addEventListener('blur', function() {
+    hideHint(input);
+  });
+});
+
+function showHint(input) {
+  const hintBox = input.nextElementSibling;
+  if (hintBox.classList.contains('hint-box')) {
+    hintBox.textContent = input.getAttribute('data-hint');
+    hintBox.style.display = 'block';
+  }
+}
+
+function hideHint(input) {
+  const hintBox = input.nextElementSibling;
+  if (hintBox.classList.contains('hint-box')) {
+    hintBox.style.display = 'none';
+  }
+}
+
+function updateOutput() {
+  const input1 = document.getElementById('input1').value;
+
+  const output = `Prompt: Transcribir la grabación de la reunión y generar un resumen, minutas y tareas para los participantes ausentes. Grabación de la reunión: ${input1}. Especificar objetivos de la reunion, temas discutidos, acciones y tareas acordadas y quien es el participante responsable a cada una de ellas. Generar un resumen de la reunion. Generar lo anterior de forma ordenada y clara. Si es necesario generar tablas.`;
+
+  outputText.innerHTML = output;
+}
+
+// Event listeners for input fields
+const inputs = document.querySelectorAll('input');
+inputs.forEach(function(input) {
+  input.addEventListener('input', function() {
+    updateOutput();
+  });
+});
+
+const copyButton = document.getElementById('copyButton');
+const promptText = document.getElementById('promptText');
+
+copyButton.addEventListener('click', function() {
+  copyToClipboard(promptText.textContent);
+});
+
+function copyToClipboard(text) {
+  const textarea = document.createElement('textarea');
+  textarea.value = text;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textarea);
+  alert('Text copied to clipboard!');
+}
+
+const copyInputsButton = document.getElementById('copyInputsButton');
+copyInputsButton.addEventListener('click', function() {
+  copyInputsAsJson();
+});
+
+function copyInputsAsJson() {
+  const inputs = document.querySelectorAll('input, textarea');
+  const inputsData = {};
+
+  inputs.forEach(function(input) {
+    inputsData[input.id] = input.value;
+  });
+
+  const json = JSON.stringify(inputsData, null, 2);
+
+  copyToClipboard(json);
+  alert('Inputs copied as JSON!');
+}
